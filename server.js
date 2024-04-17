0884df4dc3d42764a86543987e7136182878e21a
Pragya2209 <pragyasingh2209@gmail.com>
2024-04-17 23:58:09 +0530

    save

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const invoiceRoutes = require('./routes/invoice');
 const bodyParser = require('body-parser');
-const path = require('path');
 const cors = require('cors')
 
 const app = express();
@@ -13,17 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
