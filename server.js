e851ee680d03d3e5c0e230c6d4643230f575fad7
Pragya2209 <pragyasingh2209@gmail.com>
2024-04-18 00:01:06 +0530

    save

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(express.static('client/build'));
+app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
